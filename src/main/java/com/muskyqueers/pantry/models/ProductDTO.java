16a55eccc0066c122d826d773d5fc23fea30e92f
Aristotle <a.debride.2016@gmail.com>
2022-09-28 12:23:45 -0400

    pls work

--- src/main/java/com/muskyqueers/pantry/models/ProductDTO.java
+++ src/main/java/com/muskyqueers/pantry/models/ProductDTO.java
@@ -1,4 +1,12 @@
 package com.muskyqueers.pantry.models;
-
 public class ProductDTO {
+
+
+    private Integer id;
+
+    private String name;
+
+    private Long date;
+
+    private Long ex_date;
 }
