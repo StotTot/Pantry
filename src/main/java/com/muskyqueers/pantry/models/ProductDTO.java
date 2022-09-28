6c6dc4cf7f613c15fb7515a3d10a6f02b89bf417
Aristotle <a.debride.2016@gmail.com>
2022-09-28 12:26:58 -0400

    dto thingy easy

--- src/main/java/com/muskyqueers/pantry/models/ProductDTO.java
+++ src/main/java/com/muskyqueers/pantry/models/ProductDTO.java
@@ -9,4 +9,57 @@ public class ProductDTO {
     private Long date;
 
     private Long ex_date;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.date = product.getDate();
+        this.ex_date = product.getEx_date();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getDate() {
+        return date;
+    }
+
+    public void setDate(Long date) {
+        this.date = date;
+    }
+
+    public Long getEx_date() {
+        return ex_date;
+    }
+
+    public void setEx_date(Long ex_date) {
+        this.ex_date = ex_date;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date=" + date +
+                ", ex_date=" + ex_date +
+                '}';
+    }
 }
+
